bb75c131b159d787a6a2a68858b2b18a762ad59c
kkim1no <kkim2no@naver.com>
2014-12-01 12:30:01 +0900

    i want marge

--- src/main/java/org/han/web/BookController.java
+++ src/main/java/org/han/web/BookController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import javax.inject.Inject;
 
 
+
+
+import org.han.vo.PageVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/book/*")
 public class BookController {
+
+
 
 	
 	@RequestMapping("/main")
@@ -26,16 +31,12 @@ public class BookController {
 	
 	@RequestMapping("/sample")
 	public void sample(){}
-<<<<<<< HEAD
 
-=======
-	
-	// 도감 전체보기 화면 smaple list ajax로 쏴주기
 	@RequestMapping("/sample/list")
 	public @ResponseBody List<PageVO> serviceList(
 			@RequestParam(value = "page", defaultValue = "1")int page){
 		return bookService.readPage(page);
 	}
->>>>>>> 4b2a66f3da651da722e2be2a680bc6534355d5f9
+
 	
 }
