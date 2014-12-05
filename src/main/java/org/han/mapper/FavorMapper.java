5bec997e1dac531d55744b0a7465db9be0559f93
kkim1no <kkim2no@naver.com>
2014-12-05 17:51:02 +0900

    Favor view setting...

--- src/main/java/org/han/mapper/FavorMapper.java
+++ src/main/java/org/han/mapper/FavorMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.han.vo.ContVO;
 import org.han.vo.FavorVO;
 
 public interface FavorMapper {
@@ -25,4 +26,10 @@ public interface FavorMapper {
 			+" from tbl_cont c, tbl_favor f"
 			+" where userid=#{userid} and c.cno=f.cno")
 	public List<FavorVO> list(String userid);
+	
+	@Select
+	("select * from tbl_cont where cno=#{cno}")
+	public ContVO contList(Integer cno);
+
+	
 }
